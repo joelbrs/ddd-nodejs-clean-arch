7ee4a2269e1c3a9eeeb81f6baaea08185dffeee5
Joel Filho <joelfilho02812@gmail.com>
2024-04-10 11:21:08 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/sign-up.ts
+++ src/presentation/controllers/sign-up.ts
@@ -1,14 +1,16 @@
 import { MissingParamError } from "../errors/missing-param-error";
+import { badRequest } from "../helpers/http-helper";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return { statusCode: 400, body: new MissingParamError("Name") };
+      return badRequest(new MissingParamError("Name"));
     }
 
     if (!httpRequest.body.email) {
-      return { statusCode: 400, body: new MissingParamError("Email") };
+      return badRequest(new MissingParamError("Email"));
     }
+    return { body: {}, statusCode: 200 };
   }
 }
