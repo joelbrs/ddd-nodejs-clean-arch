5f8ebe0aeeabaa630c5994dbfda003e53ec4f731
Joel Filho <joelfilho02812@gmail.com>
2024-04-10 15:25:31 -0300

    test: ensure SignUpController returns 400 if password confirmation fails

--- src/presentation/controllers/sign-up.spec.ts
+++ src/presentation/controllers/sign-up.spec.ts
@@ -114,6 +114,26 @@ describe("SignUp Controller", () => {
     expect(httpResponse.body).toEqual(new InvalidParamError("email"));
   });
 
+  it("should return 400 if password confirmation fails", () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "invalid_email@mail.com",
+        password: "any_password",
+        password_confirmation: "invalid_password",
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new InvalidParamError("password_confirmation")
+    );
+  });
+
   it("should call EmailValidator with correct email", () => {
     const { sut, emailValidatorStub } = makeSut();
 
