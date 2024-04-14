4767507f9fbf67041c8d0365fe8a4248d13a8cfc
Joel Filho <joelfilho02812@gmail.com>
2024-04-13 22:18:12 -0300

    refactor: setting 12 as BCrypterAdapter's salt default

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import { Encrypter } from "../../data/protocols/encrypter";
 
 export class BCryptAdapter implements Encrypter {
-  constructor(private readonly salt: number) {}
+  constructor(private readonly salt: number = 12) {}
 
   async encrypt(value: string): Promise<string> {
     return await bcrypt.hash(value, this.salt);
